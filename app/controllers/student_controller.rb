14af7f309e469e1cee4a921f49fe0dbb2dd86d53
3nadh999 <3nadh999>
2017-10-26 11:07:20 +0530

    master branch update 26/10

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,14 +1,30 @@
 class StudentController < ApplicationController
 	def signup
+		
+
+		
+	end
+
+	def login
 		@a=params[:a]
 		@b=params[:b]
+		@h=@a+' '+@b  if params[:a]
 		@c=params[:c]
-		@d=params[:d]
+		
 		@e=params[:e]
 		@f=params[:f]
 		@g=params[:g]
+		@k=params[:k].join(",") if params[:k]
+		@j=params[:k].length() if params[:k]
 
-		puts @a
+		@d=params[:d]
+		#@l=@d.strip('%y') if params[:d]
+		
+	end
+
+	def library
+	end
+	def sports
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'student/signup'
-  post 'student/signup'
+  post 'student/login'
+  get 'student/login'
+  get 'student/library'
+  get 'student/sports'
+  root 'student#signup'
+
 
 end
